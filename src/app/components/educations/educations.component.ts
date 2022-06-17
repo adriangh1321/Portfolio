b301ca7916ec622339aa478f5564973978442118
Gustavo Hernández <adriangh1321@gmail.com>
2022-06-16 21:41:40 -0300

    added notifications of education

--- src/app/components/educations/educations.component.ts
+++ src/app/components/educations/educations.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Education } from 'src/app/models/Education';
 import { EducationService } from 'src/app/services/education.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-educations',
@@ -8,16 +10,28 @@ import { EducationService } from 'src/app/services/education.service';
   styleUrls: ['./educations.component.css']
 })
 export class EducationsComponent implements OnInit {
-  @Input() educations:Education[];
-  constructor(private educationService:EducationService) { 
-    this.educations=[]
+  @Input() educations: Education[];
+  notification!: any
+  constructor(
+    private educationService: EducationService,
+    private notificationService: NotificationService,
+    private loaderService: LoaderService) {
+    this.educations = []
   }
 
   ngOnInit(): void {
-    this.educationService.RefreshRequired.subscribe(()=>this.getEducations(parseInt(localStorage.getItem("id_portfolio")!)))
+    this.educationService.RefreshRequired.subscribe(() => this.getEducations(parseInt(localStorage.getItem("id_portfolio")!)))
+    this.notificationService.RequestNotification.subscribe((notification) => this.notification = notification)
   }
 
-  getEducations(idPortfolio:number){
-  this.educationService.getEducationsByPortfolioId(idPortfolio).subscribe(educations=>this.educations=educations)
+  getEducations(idPortfolio: number) {
+    this.educationService.getEducationsByPortfolioId(idPortfolio).subscribe({
+      next: educations => {
+        this.educations = educations
+        this.loaderService.hideLoading()
+        this.notificationService.showNotification(this.notification)
+      },
+      error: error => { }
+    })
   }
 }
--- src/app/components/show-profile/show-profile.component.ts
+++ src/app/components/show-profile/show-profile.component.ts
@@ -21,31 +21,31 @@ import { SkillService } from 'src/app/services/skill.service';
 export class ShowProfileComponent implements OnInit {
   portfolio: Portfolio;
   skillType = SkillType;
-  
+
 
   constructor(
-    private authService:AuthService,
+    private authService: AuthService,
     private portfolioService: PortfolioService,
     private experienceService: ExperienceService,
     private educationService: EducationService,
     private skillService: SkillService,
     private projectService: ProjectService,
-    private route:ActivatedRoute,
-    private loaderService:LoaderService,
-    private notificationService:NotificationService,
-    
-    ) {
+    private route: ActivatedRoute,
+    private loaderService: LoaderService,
+    private notificationService: NotificationService,
+
+  ) {
     this.portfolio = new Portfolio();
-    
-    
+
+
   }
 
   ngOnInit(): void {
-    this.portfolio=this.route.snapshot.data["portfolio"]
+    this.portfolio = this.route.snapshot.data["portfolio"]
     // this.portfolioService.getMeByToken().subscribe((res) => {
     //   this.portfolio = res
     // }) 
-  } 
+  }
 
 
 
@@ -53,21 +53,35 @@ export class ShowProfileComponent implements OnInit {
     this.loaderService.showLoading()
     const newExperience: any = { position: "Position", company: "Company", description: "Description", image: null, state: "State", country: "Country", idPortfolio: parseInt(localStorage.getItem("id_portfolio")!), startDate: new Date().toISOString().slice(0, 10) }
     this.experienceService.addExperience(newExperience).subscribe({
-      next: data => { 
-       this.notificationService.requestNotification({type:NotificationType.SUCCESS,message:NotificationMessage.EXP_ADD})
-       },
-      error: error => { 
+      next: data => {
+        this.notificationService.requestNotification(
+          {
+            type: NotificationType.SUCCESS,
+            message: NotificationMessage.EXP_ADD
+          })
+      },
+      error: error => {
         this.loaderService.hideLoading()
         throw error
-        }
+      }
     })
   }
 
   onAddEducation() {
+    this.loaderService.showLoading()
     const newEducation: any = { title: "Title", institute: "Institute", image: null, idPortfolio: parseInt(localStorage.getItem("id_portfolio")!), startDate: new Date().toISOString().slice(0, 10) }
     this.educationService.addEducation(newEducation).subscribe({
-      next: data => { alert("The education was added successfull!") },
-      error: error => { alert("There was a error"); console.log(error) }
+      next: data => {
+        this.notificationService.requestNotification(
+          {
+            type: NotificationType.SUCCESS,
+            message: NotificationMessage.EDU_ADD
+          })
+      },
+      error: error => {
+        this.loaderService.hideLoading()
+        throw error
+      }
     })
   }
 
