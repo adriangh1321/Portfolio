8b70b64935af807e803ecab66ff386bcf4bdedd3
Gustavo Hernández <adriangh1321@gmail.com>
2022-08-30 12:48:57 -0300

    refactor patchAboutMe in portfolio service

--- src/app/services/portfolio.service.ts
+++ src/app/services/portfolio.service.ts
@@ -16,8 +16,6 @@ import { ParamMap } from '@angular/router';
 })
 export class PortfolioService {
   private apiUrl: string = `${environment.baseUrl}/v1/portfolios`
-
-  // private _refreshRequired = new Subject<void>()
   private _aboutMeRefreshRequired = new Subject<void>()
   private _basicInfoRefreshRequired = new Subject<void>()
   private _portfolioRequired = new Subject<Portfolio>()
@@ -25,9 +23,6 @@ export class PortfolioService {
   private _imageRequired = new Subject<string>()
   private _basicInfoRequired = new Subject<PortfolioBasicInfo>()
 
-  // get RefreshRequired() {
-  //   return this._refreshRequired;
-  // }
 
   get AboutMeRefreshRequired() {
     return this._aboutMeRefreshRequired;
@@ -44,7 +39,6 @@ export class PortfolioService {
   get ImageRequired() {
     return this._imageRequired
   }
-
   get BasicInfoRequired() {
     return this._basicInfoRequired
   }
@@ -56,44 +50,8 @@ export class PortfolioService {
     return this.http.get<Portfolio>(url)
   }
 
-  // getPortfolioById(id: number): Observable<Portfolio> {
-  //   const url = `${this.apiUrl}/${id}`
-  //   return this.http.get<Portfolio>(url).pipe(
-  //     map(response => {
-  //       response.experiences.forEach(experience => {
-  //         if (experience.startDate !== null) {
-  //           experience.startDate = moment(experience.startDate, 'YYYY-MM-DD').toDate()
-  //         }
-
-  //         if (experience.endDate !== null) {
-  //           experience.endDate = moment(experience.endDate, 'YYYY-MM-DD').toDate()
-  //         }
-  //       })
-  //       response.educations.forEach(education => {
-  //         if (education.startDate !== null) {
-  //           education.startDate = moment(education.startDate, 'YYYY-MM-DD').toDate()
-  //         }
-
-  //         if (education.endDate !== null) {
-  //           education.endDate = moment(education.endDate, 'YYYY-MM-DD').toDate()
-  //         }
-  //       })
-
-  //       return response
-  //     }))
-  // }
-
-  // updatePortfolio(id: number, portfolio: Portfolio): Observable<void> {
-  //   const url = `${this.apiUrl}/${id}`
-  //   return this.http.put<void>(url, portfolio).pipe(
-  //     tap(() => {
-  //       this.RefreshRequired.next()
-  //     })
-  //   );
-  // }
-
-  updateAboutMe(id: number, aboutMe: any): Observable<void> {
-    const url = `${this.apiUrl}/${id}/aboutMe`
+  updateAboutMe(aboutMe: any): Observable<void> {
+    const url = `${this.apiUrl}/me/aboutMe`
     return this.http.patch<void>(url, aboutMe).pipe(
       tap(() => {
         this.AboutMeRefreshRequired.next()
