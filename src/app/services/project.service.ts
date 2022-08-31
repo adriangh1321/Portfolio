755b71df33331f0ac560b48ab8045be008b44719
Gustavo Hernández <adriangh1321@gmail.com>
2022-08-31 17:56:08 -0300

    refactor updateProject in project service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -21,7 +21,7 @@ export class ProjectService {
   constructor(private http: HttpClient) { }
 
   updateProject(id: number, project: Project): Observable<void> {
-    const url = `${this.apiUrl}/${id}`
+    const url = `${this.apiUrl}/me/${id}`
     return this.http.put<void>(url, project).pipe(
       tap(() => {
         this.RefreshRequired.next()
